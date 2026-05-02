936812808873d95839fd50d6ab57ffa0ee84ebec
Aman Chauhan <221290107009a@gmit.edu.in>
2026-05-02 12:32:35 +0530

    redirect root to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default async function DashboardPage() {
 
     return (
         <div>
-            console.log("TOTAL =", totalProducts);
+
             <h1 className="text-3xl font-bold mb-8">
                 Dashboard
             </h1>
